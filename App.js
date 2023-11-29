cb0e604ee78357cdf7d0532b7a4aa09467e31bfc
Allie Clements <allieclements@DN0a180f5f.SUNet>
2023-11-28 21:21:55 -0800

    icon test

--- App.js
+++ App.js
@@ -2,11 +2,14 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+
+// Import screens
 import LoginScreen from "./LoginScreen";
 import HomeScreen from "./HomeScreen";
 import SymptomCheckScreen from "./SymptomCheckScreen";
-import ConfirmationScreen from "./ConfirmationScreen";
-import Icon from "react-native-vector-icons/Fontisto";
+import PlaceholderScreen1 from "./PlaceholderScreen1";
+import PlaceholderScreen2 from "./PlaceholderScreen2";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -15,27 +18,32 @@ function HomeTabs() {
   return (
     <Tab.Navigator
       screenOptions={({ route }) => ({
-        tabBarIcon: ({ color, size }) => {
+        tabBarIcon: ({ focused, color, size }) => {
           let iconName;
 
           if (route.name === "Home") {
-            iconName = "home";
-          } else if (route.name === "Symptoms") {
-            iconName = "pills";
-          } // } else if (route.name === "Profile") {
-          //   iconName = "user";
-          // } else if (route.name === "Profile") {
-          //   iconName = "user";
-          // }
+            iconName = focused ? "ios-home" : "ios-home-outline";
+          } else if (route.name === "SymptomCheck") {
+            iconName = focused ? "ios-list" : "ios-list-outline";
+          } else if (route.name === "Placeholder1") {
+            iconName = focused ? "ios-add-circle" : "ios-add-circle-outline";
+          } else if (route.name === "Placeholder2") {
+            iconName = focused
+              ? "ios-information-circle"
+              : "ios-information-circle-outline";
+          }
 
-          return <Icon name={iconName} color={"#009473"} />;
+          // Return any component
+          return <Ionicons name={iconName} size={size} color={color} />;
         },
+        tabBarActiveTintColor: "#009473",
+        tabBarInactiveTintColor: "gray",
       })}
     >
       <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Symptoms" component={SymptomCheckScreen} />
-      {/* <Tab.Screen name="Perscriptions" component={HomeScreen} />
-      <Tab.Screen name="Profile" component={HomeScreen} /> */}
+      <Tab.Screen name="SymptomCheck" component={SymptomCheckScreen} />
+      <Tab.Screen name="Placeholder1" component={PlaceholderScreen1} />
+      <Tab.Screen name="Placeholder2" component={PlaceholderScreen2} />
     </Tab.Navigator>
   );
 }
@@ -49,13 +57,13 @@ export default function App() {
           component={LoginScreen}
           options={{ headerShown: false }}
         />
+        {/* HomeTabs will be the main screen after login which includes the bottom tabs */}
         <Stack.Screen
           name="HomeTabs"
           component={HomeTabs}
           options={{ headerShown: false }}
         />
-        <Stack.Screen name="SymptomCheck" component={SymptomCheckScreen} />
-        <Stack.Screen name="Confirmation" component={ConfirmationScreen} />
+        {/* No need to separately list screens that are part of the Tab Navigator */}
       </Stack.Navigator>
     </NavigationContainer>
   );
