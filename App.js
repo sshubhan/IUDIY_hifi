f1241f60dbff07f82b3efd10b004166a91c617d3
sshubhan <sshubhan@stanford.edu>
2023-11-28 21:11:56 -0800

    baseline nav bar

--- App.js
+++ App.js
@@ -2,19 +2,46 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from '@expo/vector-icons';
+
+// Import screens
 import LoginScreen from "./LoginScreen";
 import HomeScreen from "./HomeScreen";
 import SymptomCheckScreen from "./SymptomCheckScreen";
-import ConfirmationScreen from "./ConfirmationScreen";
+import PlaceholderScreen1 from "./PlaceholderScreen1"; 
+import PlaceholderScreen2 from "./PlaceholderScreen2"; 
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 function HomeTabs() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused ? 'ios-home' : 'ios-home-outline';
+          } else if (route.name === 'SymptomCheck') {
+            iconName = focused ? 'ios-list' : 'ios-list-outline';
+          } else if (route.name === 'Placeholder1') {
+            iconName = focused ? 'ios-add-circle' : 'ios-add-circle-outline';
+          } else if (route.name === 'Placeholder2') {
+            iconName = focused ? 'ios-information-circle' : 'ios-information-circle-outline';
+          }
+
+          // Return any component
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+      })}
+    >
       <Tab.Screen name="Home" component={HomeScreen} />
-      {/* Add more tabs as needed */}
+      <Tab.Screen name="SymptomCheck" component={SymptomCheckScreen} />
+      <Tab.Screen name="Placeholder1" component={PlaceholderScreen1} />
+      <Tab.Screen name="Placeholder2" component={PlaceholderScreen2} />
     </Tab.Navigator>
   );
 }
@@ -28,13 +55,13 @@ export default function App() {
           component={LoginScreen}
           options={{ headerShown: false }}
         />
+        {/* HomeTabs will be the main screen after login which includes the bottom tabs */}
         <Stack.Screen
           name="HomeTabs"
           component={HomeTabs}
           options={{ headerShown: false }}
         />
-        <Stack.Screen name="SymptomCheck" component={SymptomCheckScreen} />
-        <Stack.Screen name="Confirmation" component={ConfirmationScreen} />
+        {/* No need to separately list screens that are part of the Tab Navigator */}
       </Stack.Navigator>
     </NavigationContainer>
   );
