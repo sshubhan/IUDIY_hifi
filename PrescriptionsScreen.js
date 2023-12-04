8d34d2166d9bc7fb0f40ee44f47f70e6e416f863
Allie Clements <allieclements@Allies-MacBook-Pro-3.local>
2023-12-03 16:39:49 -0800

    Saving local changes

--- PrescriptionsScreen.js
+++ PrescriptionsScreen.js
@@ -1,17 +1,23 @@
+// PrescriptionsScreen.js
 import React from "react";
-import { View, Text, Button, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 const PrescriptionsScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      {/* Existing UI elements */}
       <Text style={styles.title}>Your Prescriptions</Text>
+
+      {/* Button to find prescription locations */}
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('PharmacyMapScreen')}>
+        <Text style={styles.buttonText}>Where to Find Prescription</Text>
+      </TouchableOpacity>
 
       {/* Button to get a new recommendation */}
       <TouchableOpacity
         style={styles.button}
-        onPress={() => navigation.navigate("RecommendationPreferences")}
-      >
+        onPress={() => navigation.navigate("RecommendationPreferences")}>
         <Text style={styles.buttonText}>Get a New Recommendation</Text>
       </TouchableOpacity>
     </View>
@@ -35,10 +41,12 @@ const styles = StyleSheet.create({
     padding: 15,
     borderRadius: 25,
     marginBottom: 20,
+    width: "80%",  // Adjust the width as needed
   },
   buttonText: {
     color: "white",
     fontSize: 18,
+    textAlign: "center", // Ensure the text is centered
   },
 });
 
