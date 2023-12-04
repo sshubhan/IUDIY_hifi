1101594e9ad941802a578d6b9659c05b4fdbeee0
sshubhan <sshubhan@stanford.edu>
2023-12-03 16:24:41 -0800

    fixing the path to the map

--- PrescriptionsScreen.js
+++ PrescriptionsScreen.js
@@ -1,18 +1,19 @@
+// PrescriptionsScreen.js
 import React from "react";
-import { View, Text, Button, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 const PrescriptionsScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      {/* Existing UI elements */}
       <Text style={styles.title}>Your Prescriptions</Text>
-
-      {/* Button to get a new recommendation */}
+      
+      {/* Other components for prescriptions can be added here */}
+      
+      {/* Button to find prescription locations */}
       <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate("RecommendationPreferences")}
-      >
-        <Text style={styles.buttonText}>Get a New Recommendation</Text>
+        style={styles.findLocationButton}
+        onPress={() => navigation.navigate('PharmacyMapScreen')}>
+        <Text style={styles.buttonText}>Where to Find Prescription</Text>
       </TouchableOpacity>
     </View>
   );
@@ -24,22 +25,25 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     padding: 20,
+    backgroundColor: "white", // Adjust the background color as needed
   },
   title: {
     fontSize: 24,
     fontWeight: "bold",
     marginBottom: 20,
   },
-  button: {
-    backgroundColor: "#009473",
-    padding: 15,
-    borderRadius: 25,
-    marginBottom: 20,
+  findLocationButton: {
+    backgroundColor: "#009473", // Adjust the button color as needed
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+    marginTop: 20,
   },
   buttonText: {
     color: "white",
-    fontSize: 18,
+    fontSize: 16,
   },
+  // Add other styles as needed
 });
 
 export default PrescriptionsScreen;
