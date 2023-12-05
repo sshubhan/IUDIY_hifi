0c140fcc3648e0d93180797aa94965a87f08df0f
Eric <emartz@stanford.edu>
2023-12-05 15:14:04 -0800

    minor styling to symptom check

--- SymptomCheckScreen.js
+++ SymptomCheckScreen.js
@@ -171,7 +171,6 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    marginBottom: 10,
   },
   separator: {
     borderBottomWidth: 1,
