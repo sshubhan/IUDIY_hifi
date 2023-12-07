f501c67243492c845fcdf28f356eade81f5688c7
sshubhan <sshubhan@stanford.edu>
2023-12-06 21:36:35 -0800

    home fix

--- NewPrescriptionConfirmationScreen.js
+++ NewPrescriptionConfirmationScreen.js
@@ -13,7 +13,10 @@ const NewPrescriptionConfirmationScreen = ({ navigation }) => {
   };
 
   const handleCancel = () => {
-    navigation.navigate("HomeScreen");
+    navigation.reset({
+      index: 0,
+      routes: [{ name: "HomeTabs" }],
+    });
   };
 
   return (
